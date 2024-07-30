dbda5908862b5868dc2fa1d9ea06172c267d58fa
samsonjisso <samsonjisso300@gmail.com>
2024-07-30 03:08:56 +0300

    initial commit

--- app/src/main/java/com/samsonjisso/phonejail/App.kt
+++ app/src/main/java/com/samsonjisso/phonejail/App.kt
@@ -1,10 +1,13 @@
 package com.samsonjisso.phonejail
 
 import android.app.Application
+import android.os.Build
+import androidx.annotation.RequiresApi
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
 
 class App: Application() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate() {
         super.onCreate()
         startKoin {
